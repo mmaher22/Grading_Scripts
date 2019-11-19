8486f4277bc57ef0ca6d8eee63de1e90cf0e6036
Mike <mikhail.papkov@gmail.com>
2019-11-19 15:39:10 +0200

    Refactor variable names in Submission.py

--- Submission.py
+++ Submission.py
@@ -27,26 +27,26 @@ class Submission:
         cmd = 'jupyter nbconvert --to notebook --inplace --ExecutePreprocessor.timeout=6000 --execute "' + self.submission_path + '"'
         out = subprocess.getoutput(cmd)
         if "Error:" in out:
-            with open("FailedNBs.txt", "a") as f:
+            with open("./logs/failed_nb.txt", "a") as f:
                 f.write(str(self.id) + '\n')
         #os.system(cmd)
         
     # Method to Find specific task solution for that user
-    def find_task(self, task_no, grade, taskFlag = '', nxtTaskFlag = '', taskDir = ''):
+    def find_task(self, task_no, grade, task_flag='', next_task_flag='', task_dir=''):
         #print('Submission Path = ', self.submission_path)
         with open(self.submission_path, 'r', encoding="utf8") as f:
             cnt_data = json.load(f)
         #Create Alert Cell For Different Solutions after rerun
         alert_cell = {"cell_type": "markdown", "metadata": {},
                        "source": ["# ALERT: Solution After Rerun is different!!! (Old Solution is Next)"]}
-        pathDiff = str(os.path.relpath(self.submission_path, start = os.path.dirname(os.path.abspath(taskDir)) )).replace('\\', '/')
+        path_diff = str(os.path.relpath(self.submission_path, start=os.path.dirname(os.path.abspath(task_dir)))).replace('\\', '/')
         #Create Solution Footer Cells
         solution_footer = {"cell_type": "markdown", "metadata": {},
                        "source": ["# StudentID:" + str(self.id) + "\n",
                                   "\n",
                                   "TrialNO:" + str(self.trial) + "\n",
                                   "\n",
-                                  "Submission:Link to [Notebook]("+ pathDiff + ")\n",
+                                  "Submission:Link to [Notebook]("+ path_diff + ")\n",
                                   "\n",
                                   "Grade:" + str(grade) + "\n",
                                   "\n",
@@ -54,36 +54,37 @@ class Submission:
                                   "\n",
                                   "\n___\n<font size = '20' color='darkgreen'> END BLOCK </font>\n___"]}
         # Flag to mark task needed
-        if taskFlag == '':
-            taskFlag = 'Task ' + str(task_no)
+        if task_flag == '':
+            task_flag = 'Task ' + str(task_no)
         # Flag to mark next task
-        if nxtTaskFlag == '':
-            nxtTaskFlag = 'Task ' + str(task_no + 1)
+        if next_task_flag == '':
+            next_task_flag = 'Task ' + str(task_no + 1)
         
         def finder(data):
             solution = []
             # Search for the task in the user submission
             flag = False
-            finishedFlag = False
+            finished_flag = False
             for cell in data['cells']:
                 #Found the task
                 try:
-                    for rowInCell in cell['source']: 
-                        if taskFlag in rowInCell:
+                    for row_in_cell in cell['source']:
+                        if task_flag in row_in_cell:
                            flag = True
                            break
                     
                     #Check if Reached next task
-                    if flag == True:
-                        for rowInCell in cell['source']:
-                            if nxtTaskFlag in rowInCell:
-                                finishedFlag = True
-                        if finishedFlag == True:
+                    if flag:
+                        for row_in_cell in cell['source']:
+                            if next_task_flag in row_in_cell:
+                                finished_flag = True
+                        if finished_flag:
                             break
                     
-                    if flag == True:
+                    if flag:
                         solution.append(cell)
-                except:
+                except Exception as e:
+                    print('Exception was raised:', e)
                     continue
             return solution
         
@@ -96,7 +97,7 @@ class Submission:
                 add_alert = True
                 break
             
-        if add_alert == True:
+        if add_alert:
             solution.append(alert_cell)
             solution.extend(old_solution)
         
